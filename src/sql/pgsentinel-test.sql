007c56d0636e022ea57e93b7926726fac2a09c3f
bdrouvot <bdrouvot@gmail.com>
2023-07-21 07:50:59 +0200

    Idle in transaction: add a test and handle v16
    Add a test for pgsentinel_ash.track_idle_trans = true
    and also make the same changes as in
    a09604e86a4c5f30b26e529539e9db018f968b87.

--- src/sql/pgsentinel-test.sql
+++ src/sql/pgsentinel-test.sql
@@ -2,5 +2,21 @@ CREATE EXTENSION pg_stat_statements;
 CREATE EXTENSION pgsentinel;
 select pg_sleep(3);
 select count(*) > 0 AS has_data from pg_active_session_history where queryid in (select queryid from pg_stat_statements);
+
+begin;
+\! sleep 3
+commit;
+
+select count(*) > 0 AS has_idle_data from pg_active_session_history where state  = 'idle in transaction';
+
+ALTER SYSTEM SET pgsentinel_ash.track_idle_trans = true;
+select pg_reload_conf();
+
+begin;
+\! sleep 3
+commit;
+
+select count(*) > 0 AS has_idle_data from pg_active_session_history where state  = 'idle in transaction';
+
 DROP EXTENSION pgsentinel;
 DROP EXTENSION pg_stat_statements;
