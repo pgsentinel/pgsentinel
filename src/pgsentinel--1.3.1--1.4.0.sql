58dd1e2717fd83a6d11625026d8a7d1657cedf2a
Bertrand Drouvot <bertranddrouvot.pg@gmail.com>
2026-02-07 09:52:39 +0000

    Stamp 1.4.0

---
+++ src/pgsentinel--1.3.1--1.4.0.sql
@@ -0,0 +1,4 @@
+/* pgsentinel--1.3.1--1.4.0.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION pgsentinel UPDATE TO '1.4.0'" to load this file. \quit
